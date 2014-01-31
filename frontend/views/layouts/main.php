2506c2f418675a820392521f49b84ccfb9bbd701
omolota <walik83@list.ru>
2014-01-31 13:47:08 +0200

    updated frontend template

--- frontend/views/layouts/main.php
+++ frontend/views/layouts/main.php
@@ -24,31 +24,16 @@
 
 </head>
 <body>
-<div class="navbar navbar-inverse navbar-fixed-top" role="navigation">
-    <div class="container">
-        <div class="navbar-header">
-            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
-                <span class="sr-only">Toggle navigation</span>
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-            </button>
-            <a class="navbar-brand" href="#">Project name</a>
-        </div>
-        <div class="collapse navbar-collapse">
-            <ul class="nav navbar-nav">
-                <li class="active"><a href="#">Home</a></li>
-                <li><a href="#about">About</a></li>
-                <li><a href="#contact">Contact</a></li>
-            </ul>
-        </div><!--/.nav-collapse -->
-    </div>
-</div>
-
+<!-- NAVIGATION BEGIN -->
+<?php $this->renderPartial('//layouts/_navigation');?>
+<!-- NAVIGATION END -->
+<!-- CONTENT WRAPPER BEGIN -->
 <div class="container">
-        <?php echo $content?>
+    <!-- CONTENT BEGIN -->
+    <?php echo $content?>
+    <!-- CONTENT END -->
 </div><!-- /.container -->
-
+<!-- CONTENT WRAPPER END -->
 <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
 <script src="https://code.jquery.com/jquery.js"></script>
 </body>
